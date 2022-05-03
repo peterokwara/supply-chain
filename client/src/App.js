8bf6705bd4cf3f0cce4c5a519888fb2fb2bf791e
Peter Okwara <peter.okwara@outlook.com>
2022-05-04 02:56:08 +0300

    Use class

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,48 @@
-import FarmDetails from './routes/Coffee/FarmDetails/FarmDetails';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
-import Header from "./components/Layout/Header/Header"
-import Footer from "./components/Layout/Footer/Footer"
-import ProductOverview from './routes/Coffee/ProductOverview/ProductOverview';
-import ProductDetails from './routes/Coffee/ProductDetails/ProductDetails';
-import TransactionHistory from './routes/Coffee/TransactionHistory/TransactionHistory';
-
-// import init from "./js/initServices";
-
-// init.initServices();
-
-function App() {
-  return (
-    <div className="App h-screen grid grid-rows-[160px,1fr,100px] lg:grid-rows-[110px,1fr,80px]">
-      <Header />
-      <Router>
-        <Routes>
-          <Route exact path="/farm-details" element={<FarmDetails />} />
-          <Route exact path="/product-overview" element={<ProductOverview />} />
-          <Route exact path="/product-details" element={<ProductDetails />} />
-          <Route exact path="/transaction-history" element={<TransactionHistory />} />
-        </Routes>
-      </Router>
-      <Footer />
-    </div >
-  );
+import FarmDetails from "./routes/Coffee/FarmDetails/FarmDetails";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Component } from "react";
+// import ServiceFactory from "./factories/serviceFactory";
+// import EthereumService from "./services/ethereumService";
+import Header from "./components/Layout/Header/Header";
+import Footer from "./components/Layout/Footer/Footer";
+import ProductOverview from "./routes/Coffee/ProductOverview/ProductOverview";
+import ProductDetails from "./routes/Coffee/ProductDetails/ProductDetails";
+import TransactionHistory from "./routes/Coffee/TransactionHistory/TransactionHistory";
+/**
+ * Main application class
+ */
+class App extends Component {
+  /**
+   * The component mounted
+   */
+  async componentDidMount() {
+    // Register the ethereum service
+    // await ServiceFactory.register("api-client", () => new EthereumService());
+  }
+  render() {
+    return (
+      <div className="App h-screen grid grid-rows-[160px,1fr,100px] lg:grid-rows-[110px,1fr,80px]">
+        <Header />
+        <Router>
+          <Routes>
+            <Route exact path="/farm-details" element={<FarmDetails />} />
+            <Route
+              exact
+              path="/product-overview"
+              element={<ProductOverview />}
+            />
+            <Route exact path="/product-details" element={<ProductDetails />} />
+            <Route
+              exact
+              path="/transaction-history"
+              element={<TransactionHistory />}
+            />
+          </Routes>
+        </Router>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
