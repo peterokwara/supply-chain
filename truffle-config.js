63b63f03d08e3601b58b7672049a10532bd55fbb
Peter Okwara <peter.okwara@outlook.com>
2022-05-05 15:10:29 +0300

    Add correct settings to deploy to rinkeby

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,15 @@
 const path = require("path");
 
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
+// Set up infura api key
+const infuraKey = "90c5f3148b7840109319eed40c9bec57";
+
+const fs = require("fs");
+
+// Import the private key for the account for the smart contract
+const privateKey = fs.readFileSync(".secret").toString().trim();
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -8,7 +18,29 @@ module.exports = {
     development: {
       host: "127.0.0.1",
       port: 8545,
-      network_id: "*"
-    }
+      network_id: "*",
+    },
+    rinkeby: {
+      provider: () =>
+        new HDWalletProvider(
+          privateKey,
+          `https://rinkeby.infura.io/v3/${infuraKey}`
+        ),
+      network_id: 4, // rinkeby's id
+      gas: 4500000, // rinkeby has a lower block limit than mainnet
+      gasPrice: 21000000000,
+      networkCheckTimeout: 1000000,
+    },
+  },
+  compilers: {
+    solc: {
+      version: "0.4.24",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200,
+        },
+      },
+    },
   },
 };
